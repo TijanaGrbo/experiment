86bf3524b29666531aadee943696ea55fea0a8f8
TijanaGrbo <tijana.grbo@gmail.com>
2020-09-25 12:40:34 +0200

    Remove commented code

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -7,13 +7,6 @@ function toggleDropdown () {
     dropdownMenu.classList.toggle('visible');
 }
 
-// // Close dropdown in window is wider than mobile breakpoint
-// window.addEventListener('resize', function () {
-//     if (window.innerWidth > 768) {
-//         closeDropdown();
-//     }
-// });
-
 // Close dropdown on scroll
 window.addEventListener('scroll', function () {
     closeDropdown();
