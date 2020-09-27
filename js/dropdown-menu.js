f32716daa1123b86a55eb9aa8e778c85257130b1
TijanaGrbo <tijana.grbo@gmail.com>
2020-09-27 22:30:40 +0200

    Restyle dropdown menu

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,16 +1,18 @@
 const dropdownBackground = document.querySelector('.login-shape');
 const dropdownMenu = document.querySelector('.dropdown-menu');
-
-// Toggle dropdown menu
-function toggleDropdown () {
-    dropdownBackground.classList.toggle('active');
-    dropdownMenu.classList.toggle('visible');
-}
+const dropdownToggle = document.querySelector('.dropdown-toggle');
 
 // Close dropdown on scroll
 window.addEventListener('scroll', function () {
     closeDropdown();
 });
+
+// Toggle dropdown menu
+function toggleDropdown () {
+    dropdownBackground.classList.toggle('active');
+    dropdownMenu.classList.toggle('visible');
+    dropdownToggle.classList.toggle('active');
+}
 
 // Create separate function for closing the dropdown
 function closeDropdown() {
